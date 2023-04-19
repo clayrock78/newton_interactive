701c6c48c5f3c09db276b92565c17c59265e6964
Clayton Jones <clayrock78@gmail.com>
2023-04-19 10:42:14 -0400

    maybe?

--- newton_interactive.py
+++ newton_interactive.py
@@ -26,16 +26,16 @@ def complex_to_pix(root:Root):
 def pix_to_complex(pos):
     x = pos[0] / WIDTH * real_width 
     y = pos[1] / HEIGHT * imag_height
-    return x,y
+    return x + 1j * y
 
 def f(x):
-    current = np.full(x.shape, np.complex64(1))
+    #current = np.full(x.shape, np.complex64(1))
     for root in roots:
-        current *= (x - root.complex)
-    return current
+        x *= (x - root.complex)
+    return x
 
 def f_prime(x):
-    step = .000001
+    step = .000000001
     return (f(x + step) - f(x)) / step
 
 # Define Colors 
@@ -74,8 +74,9 @@ while running:
             running = False
         if event.type == pg.MOUSEBUTTONDOWN:
             if mouse.get_pressed()[2]:
-                real, imag = pix_to_complex(mouse.get_pos())
-                roots.append(Root(real, imag, COLORS[cur_col] ))
+                c = pix_to_complex(mouse.get_pos())
+                real, imag = c.real, c.imag
+                roots.append(Root(real, imag, len(roots), COLORS[cur_col] ))
                 cur_col += 1
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
@@ -85,30 +86,39 @@ while running:
                 x = np.linspace(real_offset, real_width, WIDTH)
                 y = np.linspace(imag_offset, imag_height, HEIGHT)
                 reals, imags = np.meshgrid(x,y)
-                c = np.complex64(pix_to_complex((reals,imags)))
+                c = pix_to_complex((reals,imags))
+                divergence = np.ones(c.shape, dtype=bool)
+                diverged_roots = np.zeros(c.shape, dtype=int)
                 #print(c.shape)
                 #for _ in range(ITERATIONS):
                 #    c = c - f(c) / f_prime(c)
                 
-                color_shape = [300,300,3]
-                nearest_color = np.empty((300,300,3))
-                nearest_distance = np.full((300,300), np.inf)
-                print(c.shape)
-                for root in roots:
-                    distance = distance_between(c[0], np.full(c[0].shape,root.complex))
-                    mask = np.greater(nearest_distance, distance)
-                    nearest_distance = np.where(mask, distance, nearest_distance)
-                    mask.shape = (300,300,1)
-                    nearest_color = np.where(mask, np.full((300,300,3),np.array(root.color)), nearest_color)
-                
+                for _ in range(ITERATIONS):
+                    c[divergence] = c[divergence] - f(c[divergence]) / f_prime(c[divergence])
+                    for root in roots: 
+                        #print(abs(root.complex+c))
+                        #print(abs(root.complex-c))
+                        print(abs(c-root.complex))
+                        # print(abs(c+root.complex))
+                        print(c)
 
+                        diverged = np.greater(.1, abs(c-root.complex), out=np.full(c.shape,False), where=divergence)
+                        diverged_roots[not diverged] = root.index
+                        divergence[diverged] = False
+                    #print(c)
+                    print([root.complex for root in roots])
+                
+                print(diverged_roots)
+                colors = np.array([root.color for root in roots])[diverged_roots.astype(int)]
                 # for indx, x in enumerate(c):
                 #     for indy, y in enumerate(x):
                 #         for ind, comp in enumerate(y):
                 #             color = np.array(list(min(roots, key=lambda x:distance_between(x.complex, comp)).color))
                 #             colors[indy][indx] = color
                 #print(colors)
-                fractal_surface = pg.surfarray.make_surface(nearest_color)
+
+
+                fractal_surface = pg.surfarray.make_surface(colors)
 
     #3 Draw/render
     screen.fill(BLACK)
@@ -121,7 +131,7 @@ while running:
 
         if mouse.get_pressed()[0]:
             if current_root == root:
-                c = complex(*pix_to_complex(mouse.get_pos()))
+                c = pix_to_complex(mouse.get_pos())
                 root.complex = c
                 root.real = c.real
                 root.imag = c.imag
--- root.py
+++ root.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 class Root():
-    def __init__(self, real, imag, color=(0,0,0,)):
+    def __init__(self, real, imag, index, color=(0,0,0,)):
         self.real = real
         self.imag = imag
         self.complex = np.complex64(real + imag * 1j)
         self.color = color
         self.radius = 20
+        self.index = index
