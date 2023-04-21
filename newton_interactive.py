fe87484d2cfc82672607a648d3a4f8bf030f62c4
Clayton Jones <clayrock78@gmail.com>
2023-04-21 11:40:58 -0400

    ACTUALLY WORKS

--- newton_interactive.py
+++ newton_interactive.py
@@ -8,9 +8,9 @@ real_offset = 0
 imag_height = 5
 imag_offset = 0
 
-ITERATIONS = 10
-WIDTH = 300
-HEIGHT = 300
+ITERATIONS = 20
+WIDTH = 800
+HEIGHT = 800
 
 def distance_between(pos1, pos2):
     return ((pos1.real - pos2.real)**2 + (pos1.imag - pos2.imag)**2)**.5
@@ -29,13 +29,13 @@ def pix_to_complex(pos):
     return x + 1j * y
 
 def f(x):
-    #current = np.full(x.shape, np.complex64(1))
+    current = np.full(x.shape, np.complex64(1))
     for root in roots:
-        x *= (x - root.complex)
-    return x
+        current *= (x - root.complex)
+    return current
 
 def f_prime(x):
-    step = .000000001
+    step = .00001
     return (f(x + step) - f(x)) / step
 
 # Define Colors 
@@ -83,42 +83,33 @@ while running:
                 running = False
             if event.key == pg.K_SPACE:
                 # form 2 2D arrays to store the real and imag
-                x = np.linspace(real_offset, real_width, WIDTH)
-                y = np.linspace(imag_offset, imag_height, HEIGHT)
-                reals, imags = np.meshgrid(x,y)
-                c = pix_to_complex((reals,imags))
-                divergence = np.ones(c.shape, dtype=bool)
-                diverged_roots = np.zeros(c.shape, dtype=int)
-                #print(c.shape)
-                #for _ in range(ITERATIONS):
-                #    c = c - f(c) / f_prime(c)
-                
-                for _ in range(ITERATIONS):
-                    c[divergence] = c[divergence] - f(c[divergence]) / f_prime(c[divergence])
-                    for root in roots: 
-                        #print(abs(root.complex+c))
-                        #print(abs(root.complex-c))
-                        print(abs(c-root.complex))
-                        # print(abs(c+root.complex))
-                        print(c)
+                x = np.linspace(real_offset, real_offset + real_width, WIDTH)
+                y = np.linspace(imag_offset, imag_offset + imag_height, HEIGHT)
+                reals, imags = np.meshgrid(x, y)
+                c = reals + 1j*imags
+                colors = np.zeros((WIDTH, HEIGHT, 3), dtype=np.uint8)
+                calculated = np.zeros((WIDTH, HEIGHT), dtype=bool)
 
-                        diverged = np.greater(.1, abs(c-root.complex), out=np.full(c.shape,False), where=divergence)
-                        diverged_roots[not diverged] = root.index
-                        divergence[diverged] = False
-                    #print(c)
-                    print([root.complex for root in roots])
-                
-                print(diverged_roots)
-                colors = np.array([root.color for root in roots])[diverged_roots.astype(int)]
-                # for indx, x in enumerate(c):
-                #     for indy, y in enumerate(x):
-                #         for ind, comp in enumerate(y):
-                #             color = np.array(list(min(roots, key=lambda x:distance_between(x.complex, comp)).color))
-                #             colors[indy][indx] = color
-                #print(colors)
+                # calculate the fractal
+                for i in range(ITERATIONS):
+                    not_calculated = np.logical_not(calculated)
+                    c[not_calculated] = c[not_calculated] - f(c[not_calculated]) / f_prime(c[not_calculated])
+                    if i % 5 == 0:
+                        for root in roots:
+                            # if the root is close enough, set the color
+                            mask = (c.real - root.real)**2 + (c.imag - root.imag)**2 < .01
+                            colors[mask] = np.array(root.color)
+                            calculated[mask] = True
 
+                for root in roots:
+                    # if the root is close enough, set the color
+                    mask = (c.real - root.real)**2 + (c.imag - root.imag)**2 < .01
+                    colors[mask] = np.array(root.color)
+                    calculated[mask] = True
+            
+                # draw the fractal to fractal_surface
+                pg.surfarray.blit_array(fractal_surface, colors)
 
-                fractal_surface = pg.surfarray.make_surface(colors)
 
     #3 Draw/render
     screen.fill(BLACK)
